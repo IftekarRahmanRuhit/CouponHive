bd8b20803dd17b8d6f22b0ae034298879e7fdca3
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2024-11-18 15:43:41 +0600

    initial setup done

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: [
+      "winter",
+      "cyberpunk",
+      "coffee" // This enables the winter theme
+    ],
+  },
+};
+
